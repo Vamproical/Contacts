2b55fa1615f134facc77b588c5d9f68851f74afa
Nick <mimoun136@yandex.ru>
2020-08-26 15:37:50 +0900

    Finish the stage 1.

--- Contacts/task/src/contacts/Main.java
+++ Contacts/task/src/contacts/Main.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Enter the name of the person:");
+        String name = scanner.nextLine();
+        System.out.println("Enter the surname of the person:");
+        String surname = scanner.nextLine();
+        System.out.println("Enter the number:");
+        String number = scanner.nextLine();
+        System.out.println();
+        System.out.println("A record created!");
+        System.out.println("A Phone Book with a single record created!");
     }
 }
